05b675a86653a424b0af19f2bd1e04db4a104d92
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-01-13 14:20:27 +0530

    Adding Event Listeners with jQuery

--- JQuery/index.js
+++ JQuery/index.js
@@ -1,14 +1,33 @@
 // document.querySelector("h1").style.color = "red";
 
+// // Selecting Elements with jQuery
 // $("h1").css("color","red");
 // or
 // jQuery("h1").css("color","red");
 
+// Manipulating Styles with jQuery
 $("h1").addClass("heading margin-5");
 
+// // Manipulating Text with jQuery
 // $("h1").text("Good bye");
-
 // $("button").text("Dont click me");
 // $("button").html("<em>Dont click me<em/>");
-$("a").attr("href","https://www.google.com");
-console.log($("img").attr("src"));
+
+// // Manipulating Attributes with jQuery
+// $("a").attr("href","https://www.google.com");
+// console.log($("img").attr("src"));
+
+// Adding Event Listeners with jQuery
+$("button").click(function (){
+    // $("h1").slideUp(1000);
+    $("h1").css("color","purple");
+});
+
+$("input").keypress(function (event){
+    console.log(event.key);
+    $("h1").text(event.key);
+});
+
+$("h1").on("mouseover", function (){
+    $("h1").css("color","purple");
+});
