d83b9b0e4f77d1781e01e04521cef29ccbdbf313
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-01-13 13:44:30 +0530

    Manipulating Text with jQuery

--- JQuery/index.js
+++ JQuery/index.js
@@ -5,3 +5,8 @@
 // jQuery("h1").css("color","red");
 
 $("h1").addClass("heading margin-5");
+
+$("h1").text("Good bye");
+
+// $("button").text("Dont click me");
+$("button").html("<em>Dont click me<em/>");
