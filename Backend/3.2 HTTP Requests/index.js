a46230c16ed0a1f62949f92c4e68e1cb1e71d682
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-01-15 19:41:43 +0530

    HTTP Response Status Codes

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -7,6 +7,14 @@ app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
 
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact</h1><p>Contact me ig:kishore2106</p>");
+});
+
+app.get("/about", (req, res) => {
+    res.send("<h1>About Me</h1><p>My name is Kishore</p>");
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
--- README.md
+++ README.md
@@ -180,3 +180,9 @@ Important links - (https://github.com/rwaldron/idiomatic.js)
 4. HTTP Requests.
 5. Postman.
 6. Introduction to Middleware.
+7. HTTP Response Status Codes.
+    1. Informational responses (100-199)
+    2. Successful responses (200-299)
+    3. Redirection messages (300-399)
+    4. Client error responses (400-499)
+    5. Server error responses (500-599)
